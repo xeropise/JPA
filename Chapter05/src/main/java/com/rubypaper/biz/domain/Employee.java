e2a02606c1883fe1c925581f979e3f577ddef4f1
xeropise <whrbql1@gmail.com>
2021-06-06 15:08:49 +0900

    5th June

---
+++ Chapter05/src/main/java/com/rubypaper/biz/domain/Employee.java
@@ -0,0 +1,32 @@
+package com.rubypaper.biz.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "S_EMP")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 25, nullable = false)
+	private String name;
+	
+	@OneToOne(mappedBy = "employee")
+	private EmployeeCard card;
+	
+	public void setEmployeeCard(EmployeeCard card) {
+		this.card = card; 
+		card.setEmployee(this);
+	}
+}
---
+++ Chapter05/src/main/java/com/rubypaper/biz/domain/EmployeeCard.java
@@ -0,0 +1,36 @@
+package com.rubypaper.biz.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString(exclude="employee")
+@Entity
+@Table(name = "S_EMP_CARD")
+public class EmployeeCard {
+
+	@Id
+	@Column(name = "CARD_ID")
+	private Long cardId;		// 사원증 아이디
+	
+	@Column(name = "EXPIRE_DATE")
+	private Date expireDate;		// 사원증 만료 기간
+	
+	private String role;		// 권한
+	
+	@MapsId
+	@OneToOne
+	@JoinColumn(name = "EMP_ID")
+	private Employee employee;
+	
+}
